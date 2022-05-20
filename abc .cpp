8041fc1ab29b7caae2eea7d640e20eeebc5bbc4a
JaveriaJarral <103873464+JaveriaJarral@users.noreply.github.com>
2022-05-21 04:39:16 +0500

    Create abc .cpp

---
+++ abc .cpp
@@ -0,0 +1,20 @@
+ #include<iostream>
+ using namespace std;
+ int main()
+ {
+  int a=5;
+  int b=10;
+  int c=15;
+if (a==b && b==c)
+{
+	cout<<"values are equal";
+}
+if (a!=b && b!=c)
+{
+	cout<<"values are not equal";
+}
+else 
+{
+	cout<<"print else";
+}
+}
